fd654133f905008d33a08976ff3b8cbbb8e47254
Michael Williamson <michaelrwilliamson92@gmail.com>
2020-02-13 15:35:39 +0000

    started basic front end, home page idea is partially in place via state 'showmore' toggle. will need to improve visual as well as add routes etc.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+export default class App extends React.Component {
+
+  state = {
+    showMore : false
+  }
+
+  handleClick = () => {
+    this.setState({
+      showMore : !this.state.showMore
+    })
+  }
+
+  render() {
+    return(
+
+      // <div>
+      //   <Switch>
+        // <Route path="/newGame" component={newGame}></Route>
+        // <Route path="/continue" component={continueGame}></Route>
+        /* <Link key={"newGame"} to={"/newGame"}>
+           <li>New Game</li>
+           </Link> */
+           /* </Switch>
+      </div> */
+
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div className="container">
+         <h1> Welcome to the world of Pokemon!</h1>
+         <button className="button1" onClick={this.handleClick}> Start </button>
+         {this.state.showMore === true ? (
+         <ul>
+           <li>
+           New Game
+           </li>
+           <li>
+             Continue
+           </li>
+         </ul> )
+         : null }
+       </div>
       </header>
     </div>
+  
   );
+};
 }
 
-export default App;
