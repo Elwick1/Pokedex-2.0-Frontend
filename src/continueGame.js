8cf8cc236d27ad7c350298189d08d9e21f41cc92
Michael Williamson <michaelrwilliamson92@gmail.com>
2020-02-24 17:16:27 +0000

    started basic version of contiune page

--- src/continueGame.js
+++ src/continueGame.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import './App.css';
+
+export default class ContinueGame extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1> Welcome back! </h1>
+                <h2> please log in below with your trainer Id and password </h2>
+                
+            </div>
+        )
+    }
+
+}
