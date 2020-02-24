7ae7ff97211349e57c63bbf81891aadc0c7193db
Michael Williamson <michaelrwilliamson92@gmail.com>
2020-02-24 17:14:14 +0000

    startd work on new game page

--- src/newGame.js
+++ src/newGame.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './App.css';
+
 
 export default class NewGame extends React.Component {
     render() {
         return(
             <div>
                 <h1>New Game</h1>
+                <h2> we need some information aout you before we can begin with our registration process </h2>
+                <li>
+                    Boy
+                </li>
+                <li>
+                    Girl
+                </li>
             </div>
         )
     }
